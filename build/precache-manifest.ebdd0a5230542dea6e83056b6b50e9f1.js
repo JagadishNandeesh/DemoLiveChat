7ae77841b3987516e2205ad03306b833d167d634
Jagadish_N <jagadish.n@sonata-software.com>
2019-11-18 14:23:59 +0000

    Practice live chat 1st commit

---
+++ build/precache-manifest.ebdd0a5230542dea6e83056b6b50e9f1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "12d2f75c23a34a189532",
+    "url": "/static/js/main.12d2f75c.chunk.js"
+  },
+  {
+    "revision": "03c3804354b917574fa9",
+    "url": "/static/js/1.03c38043.chunk.js"
+  },
+  {
+    "revision": "12d2f75c23a34a189532",
+    "url": "/static/css/main.5ea9c5c1.chunk.css"
+  },
+  {
+    "revision": "2cf4cb0b60ee498172165b35ed3317df",
+    "url": "/index.html"
+  }
+];
---
+++ src/reducers/messages.js
@@ -0,0 +1,19 @@
+const messages = (state = [], action) => {
+  console.log(action);
+  switch (action.type) {
+    case "ADD_MESSAGE":
+    case "MESSAGE_RECEIVED":
+      return state.concat([
+        {
+          message: action.message,
+          author: action.author,
+          id: action.id
+        }
+      ]);
+
+    default:
+      return state;
+  }
+};
+
+export default messages;
